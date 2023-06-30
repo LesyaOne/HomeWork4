9719ad7e6dd6c627732f55d1233cdb29d0a2a428
lesya_popova <lspopova99@gmail.com>
2023-07-01 05:10:11 +0800

    t2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumOfDigits (int num)
+{
+    int sum = 0;
+    while (num >=10)
+    {
+        sum = sum + (num % 10);
+        num /= 10;
+    }
+    sum = sum + num;
+    return sum;
+}
+Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number = number * (-1);
+Console.WriteLine($"Sum of digits in your number is {SumOfDigits(number)}");
