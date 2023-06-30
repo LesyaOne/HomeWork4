0ec852fd39df4e6e6d5275166c62ad0691b3f111
lesya_popova <lspopova99@gmail.com>
2023-07-01 04:43:45 +0800

    t1

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿int Stepen (int numA, int numB)
+{
+    int result = 1;
+    for (int i=0; i<numB; i++)
+    {
+        result = result * numA;
+    }
+    return result;
+}
+Console.Write ("Enter number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Enter number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+while (b<0)
+{
+    Console.Write("Enter number B again: ");
+    b = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"A in degree B is {Stepen(a, b)}");
